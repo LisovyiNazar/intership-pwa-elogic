68db337fececfc8d74309e122972e8960fa1df87
Lisovyi Nazar <na3alis@gmail.com>
2022-10-20 16:58:55 +0300

    change href for toast

--- src/components/Notifications/notifications.js
+++ src/components/Notifications/notifications.js
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast';
 const Notifications = ({ isAddToCartDisabled }) => {
     if (!isAddToCartDisabled) {
         return (
-            <a href='/checkout'>
+            <a href='/cart'>
                 <Toaster toastOptions={{ duration: 5000, position: 'top-center' }} />
             </a>
         );
